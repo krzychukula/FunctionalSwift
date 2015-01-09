cc2971d25023afb641bdf9916a0dd331f52dff6b
krzychukula <krzychukula@gmail.com>
2015-01-09 14:40:29 +0100

    add minimum distance

--- Functional1.playground/section-1.swift
+++ Functional1.playground/section-1.swift
@@ -15,3 +15,13 @@ func inRange2(target:Position, ownPosition:Position, range: Distance) -> Bool {
     let targetDistance = sqrt(dx * dx + dy * dy)
     return targetDistance <= range
 }
+
+let minimumDistance: Distance = 2.0
+
+func inRange3(target: Position, ownPosition: Position, range: Distance) -> Bool {
+    let dx = ownPosition.x - target.x
+    let dy = ownPosition.y - target.y
+    let targetDistance = sqrt(dx * dx + dy * dy)
+    return targetDistance <= range
+            && targetDistance >= minimumDistance
+}
