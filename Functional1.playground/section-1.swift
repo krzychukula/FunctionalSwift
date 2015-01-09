82a6b2f641af34018709dadc582aae506a118dc9
krzychukula <krzychukula@gmail.com>
2015-01-09 14:37:59 +0100

    Initial commit
    My first idea how to write examples from Functional Swift

---
+++ Functional1.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+typealias Position = CGPoint
+typealias Distance = CGFloat
+
+func inRange1(target:Position, range: Distance) -> Bool {
+    return sqrt(target.x * target.y + target.y * target.y * target.y) <= range
+}
+
+func inRange2(target:Position, ownPosition:Position, range: Distance) -> Bool {
+    let dx = ownPosition.x - target.x
+    let dy = ownPosition.y - target.y
+    let targetDistance = sqrt(dx * dx + dy * dy)
+    return targetDistance <= range
+}
