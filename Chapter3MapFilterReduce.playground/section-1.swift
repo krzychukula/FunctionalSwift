52b9a537d5f6bb643efccd0ee6849139c4e11bed
krzychukula <krzychukula@gmail.com>
2015-01-11 10:43:55 +0100

    generic Map funcion

--- Chapter3MapFilterReduce.playground/section-1.swift
+++ Chapter3MapFilterReduce.playground/section-1.swift
@@ -29,3 +29,27 @@ func computeIntArray(xs: [Int], f: Int -> Int) -> [Int] {
 func doubleArray2(xs: [Int]) -> [Int] {
     return computeIntArray(xs) { x in x * 2 }
 }
+
+//bool type error
+//func isEvenArray(xs: [Int]) -> [Bool] {
+//    computeIntArray(xs)) { x in x % 2 == 0 }
+//}
+//
+//isEvenArray([1])
+
+func genericComputeArray<U>(xs: [Int], f: Int -> U) -> [U] {
+    var result: [U] = []
+    for x in xs {
+        result.append(f(x))
+    }
+    return result
+}
+
+func map<T, U>(xs: [T], f: T -> U) -> [U] {
+    var result: [U] = []
+    for x in xs {
+        result.append(f(x))
+    }
+    return result
+}
+
