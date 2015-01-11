4132413df82c090110f9356c52644476f13c5fb5
krzychukula <krzychukula@gmail.com>
2015-01-11 10:35:10 +0100

    chapter 3 - specific functions with for loops

---
+++ Chapter3MapFilterReduce.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+func incrementArray(xs: [Int]) -> [Int] {
+    var result: [Int] = []
+    for x in xs {
+        result.append(x + 1)
+    }
+    return result
+}
+
+func doubleArray1(xs: [Int]) -> [Int] {
+    var result: [Int] = []
+    for x in xs {
+        result.append(x * 2)
+    }
+    return result
+}
