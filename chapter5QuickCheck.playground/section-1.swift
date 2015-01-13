2d57c1c25980b4792c186b60d83621f565428a05
krzychukula <krzychukula@gmail.com>
2015-01-13 21:02:35 +0100

    arbitrary string requires more work

--- chapter5QuickCheck.playground/section-1.swift
+++ chapter5QuickCheck.playground/section-1.swift
@@ -33,3 +33,34 @@ extension Int: Arbitrary {
 }
 
 Int.arbitrary()
+
+extension Character: Arbitrary {
+    static func arbitrary() -> Character {
+        return Character(UnicodeScalar(random(from: 65, to: 90)))
+    }
+    
+    func smaller() -> Character? { return nil }
+}
+
+func random(#from: Int, #to: Int) -> Int {
+    return from + (Int(arc4random()) % (to - from))
+}
+
+func tabulate<A>(times: Int, f: Int -> A) -> [A] {
+    return Array(0 ..< times).map(f)
+}
+
+extension String: Arbitrary {
+    static func arbitrary() -> String {
+        let randomLength = random(from:1, to:40)
+        let randomCharacters = tabulate(randomLength) { _ in
+            Character.arbitrary()
+        }
+        return randomCharacters.reduce("", combine: { $0 + String($1)})
+    }
+}
+
+String.arbitrary()
+
+
+
