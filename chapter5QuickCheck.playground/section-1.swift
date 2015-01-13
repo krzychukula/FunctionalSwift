169c882574d2c1f7022e85a9e3f6dc5553b16d34
krzychukula <krzychukula@gmail.com>
2015-01-13 21:47:56 +0100

    Shrink data if counterexample found

--- chapter5QuickCheck.playground/section-1.swift
+++ chapter5QuickCheck.playground/section-1.swift
@@ -115,8 +115,37 @@ check1("Every string starts with Hello") { (s: String) in
 }
 
 
+func iterateWhile<A>(condition: A -> Bool,
+    initialValue: A,
+    next: A -> A?) -> A {
+        if let x = next(initialValue) {
+            if condition(x) {
+                return iterateWhile(condition, x, next)
+            }
+        }
+        return initialValue
+}
+
+func check2<A: Arbitrary>(message: String, prop: A -> Bool) -> () {
+    for _ in 0..<numberOfIterations {
+        let value = A.arbitrary()
+        if !prop(value) {
+            let smallerValue = iterateWhile({ !prop($0) }, value) {
+                $0.smaller()
+            }
+            println("-\(message)- doesn't hold: \(smallerValue)")
+            return
+        }
+    }
+    println("\"\(message)\" passed \(numberOfIterations) tests.")
+}
 
 
+check2("Area should be at least 0") { size in area(size) >= 0 }
 
+
+check2("Every string starts with Hello") { (s: String) in
+    s.hasPrefix("Hello")
+}
 
 
