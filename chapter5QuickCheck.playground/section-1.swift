50c19ce169b1c07e0cb72ed7015b3637461768cd
krzychukula <krzychukula@gmail.com>
2015-01-13 21:07:04 +0100

    simple check funcion

--- chapter5QuickCheck.playground/section-1.swift
+++ chapter5QuickCheck.playground/section-1.swift
@@ -62,5 +62,16 @@ extension String: Arbitrary {
 
 String.arbitrary()
 
+let numberOfIterations = 5
 
+func check1<A: Arbitrary>(message: String, prop: A -> Bool) -> () {
+    for _ in 0 ..< numberOfIterations {
+        let value = A.arbitrary()
+        if !prop(value) {
+            println("\"\(message)\" doesn't hold: \(value)")
+            return
+        }
+    }
+    println("\"\(message)\" passed \(numberOfIterations) tests.")
+}
 
