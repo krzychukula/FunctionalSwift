d6e52c32fda959e64bd0989303a9de6f333429e3
krzychukula <krzychukula@gmail.com>
2015-01-14 21:01:51 +0100

    struct is a value type

--- Chapter6Immutability.playground/section-1.swift
+++ Chapter6Immutability.playground/section-1.swift
@@ -4,3 +4,19 @@ import Cocoa
 
 var x: Int = 1
 let y: Int = 2
+
+x = 3
+//y = 4
+
+struct PointStruct {
+    var x: Int
+    var y: Int
+}
+
+var structPoint = PointStruct(x: 1, y: 2)
+var sameStructPoint = structPoint
+sameStructPoint.x = 3
+structPoint
+
+
+
