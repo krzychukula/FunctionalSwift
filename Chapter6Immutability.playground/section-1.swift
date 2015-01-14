d0b754bfb398e54f0cd455487c1e35d487cc5c6a
krzychukula <krzychukula@gmail.com>
2015-01-14 21:12:17 +0100

    structs and classes as agruments

--- Chapter6Immutability.playground/section-1.swift
+++ Chapter6Immutability.playground/section-1.swift
@@ -32,3 +32,27 @@ var classPoint = PointClass(x: 1, y: 2)
 var sameClassPoint = classPoint
 sameClassPoint.x = 3
 
+
+func setStructToOrigin(var point: PointStruct) -> PointStruct {
+    //struct is copied
+    point.x = 0
+    point.y = 0
+    return point
+}
+
+var structOrigin: PointStruct = setStructToOrigin(structPoint)
+structPoint
+
+
+func setClassToOrigin(point: PointClass) -> PointClass {
+    //changes passed object
+    point.x = 0
+    point.y = 0
+    return point
+}
+
+var classOrigin = setClassToOrigin(classPoint)
+
+
+
+
