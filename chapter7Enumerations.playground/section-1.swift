fe7e94d4b13add3c62214384299bd0405a12a097
krzychukula <krzychukula@gmail.com>
2015-01-15 08:42:06 +0100

    simple read file function

--- chapter7Enumerations.playground/section-1.swift
+++ chapter7Enumerations.playground/section-1.swift
@@ -57,4 +57,20 @@ func localizedEncodingName(encoding: Encoding) -> String {
 localizedEncodingName(Encoding.NEXTSTEP)
 
 
+//Associated Values
+
+func readFile1(path: String, encoding: Encoding) -> String? {
+    var maybeError: NSError? = nil
+    let maybeString = NSString(contentsOfFile: path, encoding: toNSStringEncoding(encoding), error: &maybeError)
+    return maybeString
+}
+
+
+
+
+
+
+
+
+
 
