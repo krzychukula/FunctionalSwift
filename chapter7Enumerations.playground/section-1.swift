e7f43454bdd9618c93ea196b757e45621be0490f
krzychukula <krzychukula@gmail.com>
2015-01-15 21:31:00 +0100

    write file naive impl

--- chapter7Enumerations.playground/section-1.swift
+++ chapter7Enumerations.playground/section-1.swift
@@ -96,5 +96,11 @@ case let ReadFileResult.Failure(error):
 
 
 
+//Adding Generics
+
+func writeFile(contents: String, path: String, encoding: Encoding) -> Bool {
+    return contents.writeToFile(path, atomically: false, encoding: toNSStringEncoding(encoding), error: nil)
+}
+
 
 
