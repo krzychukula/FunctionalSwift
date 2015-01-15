13099a147cd99871ce2708ff0d8400dcdc6b9366
krzychukula <krzychukula@gmail.com>
2015-01-15 08:35:59 +0100

    Switch case in opposite direction

--- chapter7Enumerations.playground/section-1.swift
+++ chapter7Enumerations.playground/section-1.swift
@@ -33,3 +33,19 @@ func toNSStringEncoding(encoding: Encoding) -> NSStringEncoding {
         return NSUTF8StringEncoding
     }
 }
+
+func createEncoding(enc: NSStringEncoding) -> Encoding? {
+    switch enc {
+    case NSASCIIStringEncoding:
+        return Encoding.ASCII
+    case NSNEXTSTEPStringEncoding:
+        return Encoding.NEXTSTEP
+    case NSJapaneseEUCStringEncoding:
+        return Encoding.JapaneseEUC
+    case NSUTF8StringEncoding:
+        return Encoding.UTF8
+    default:
+        return nil
+    }
+}
+
