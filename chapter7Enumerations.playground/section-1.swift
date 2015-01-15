87b9dd69c8101d1136495caa01e4a069eeae709d
krzychukula <krzychukula@gmail.com>
2015-01-15 08:47:30 +0100

    better read file that returns error

--- chapter7Enumerations.playground/section-1.swift
+++ chapter7Enumerations.playground/section-1.swift
@@ -72,7 +72,19 @@ enum ReadFileResult {
 
 let exampleSuccess: ReadFileResult = ReadFileResult.Success("File contents goes here")
 
-
+func readFile(path: String, encoding: Encoding) -> ReadFileResult {
+    var maybeError: NSError?
+    let maybeString: String? = NSString(contentsOfFile: path, encoding: toNSStringEncoding(encoding), error: &maybeError)
+    
+    if let string = maybeString {
+        return ReadFileResult.Success(string)
+    } else if let error = maybeError {
+        return ReadFileResult.Failure(error)
+    } else {
+        assert(false, "The impossible occurred")
+    }
+    
+}
 
 
 
