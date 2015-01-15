77642cb8b7a2a901e66dccfd187a46b4315002ff
krzychukula <krzychukula@gmail.com>
2015-01-15 08:27:56 +0100

    Enumerations in objc and swift

---
+++ chapter7Enumerations.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+//OBJ-C
+//enum NSStringEncoding {
+//    NSASCIIStringEncoding = 1,
+//    NSNEXTSTEPStringEncoding = 2,
+//    NSJapaneseEUCStringEncoding = 3,
+//    NSUTF8StringEncoding = 4
+//}
+
+enum Encoding {
+    case ASCII
+    case NEXTSTEP
+    case JapaneseEUC
+    case UTF8
+}
+
