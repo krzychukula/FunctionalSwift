ff477da212c1fe3002ee9196e2e5a2125464c1a9
krzychukula <krzychukula@gmail.com>
2015-01-15 08:32:57 +0100

    Use enums in switch statements

--- chapter7Enumerations.playground/section-1.swift
+++ chapter7Enumerations.playground/section-1.swift
@@ -20,3 +20,16 @@ enum Encoding {
 }
 
 //let myEncoding = Encoding.ASCII + Encoding.UTF8
+
+func toNSStringEncoding(encoding: Encoding) -> NSStringEncoding {
+    switch encoding {
+    case Encoding.ASCII:
+        return NSASCIIStringEncoding
+    case Encoding.NEXTSTEP:
+        return NSNEXTSTEPStringEncoding
+    case Encoding.JapaneseEUC:
+        return NSJapaneseEUCStringEncoding
+    case Encoding.UTF8:
+        return NSUTF8StringEncoding
+    }
+}
