b2ba046b38ca3e9d8e36992822f5cb81bb3b1add
krzychukula <krzychukula@gmail.com>
2015-01-16 08:36:46 +0100

    own map and ?? operator

--- chapter7Enumerations.playground/section-1.swift
+++ chapter7Enumerations.playground/section-1.swift
@@ -147,3 +147,29 @@ func writeFile(contents: String,
 }
 
 
+//Optionals Revisited
+
+enum Optional<T> {
+    case None
+    case Some(T)
+    //...
+}
+
+
+func map<T, U>(f: T -> U, result: Result<T>) -> Result<U> {
+    switch result {
+    case let Result.Success(box):
+        return Result.Success(Box(f(box.unbox)))
+    case let Result.Failure(error):
+        return Result.Failure(error)
+    }
+}
+
+func ??<T>(result: Result<T>, handleError: NSError -> T) -> T {
+    switch result {
+    case let Result.Success(box):
+        return box.unbox
+    case let Result.Failure(error):
+        return handleError(error)
+    }
+}
