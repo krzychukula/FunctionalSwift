e8ac25b51d9e7d37cee539e20fe217e632be353e
krzychukula <krzychukula@gmail.com>
2015-01-17 19:43:22 +0100

    simple impl

--- chapter8PurelyFunctionalDataStructures.playground/section-1.swift
+++ chapter8PurelyFunctionalDataStructures.playground/section-1.swift
@@ -2,7 +2,21 @@
 
 import Cocoa
 
+func emptySet<T>() -> Array<T> {
+    return []
+}
 
+func isEmptySet<T>(set: [T]) -> Bool {
+    return set.isEmpty
+}
+
+func setContains<T: Equatable>(x: T, set: [T]) -> Bool {
+    return contains(set, x)
+}
+
+func setInsert<T: Equatable>(x: T, set: [T]) -> [T] {
+    return setContains(x, set) ? set : [x] + set
+}
 
 
 
