e1c3634d03ac5439882ca5ad2c813b1a5f2086d6
krzychukula <krzychukula@gmail.com>
2015-01-17 20:18:03 +0100

    simple tree structure

--- chapter8PurelyFunctionalDataStructures.playground/section-1.swift
+++ chapter8PurelyFunctionalDataStructures.playground/section-1.swift
@@ -18,10 +18,20 @@ func setInsert<T: Equatable>(x: T, set: [T]) -> [T] {
     return setContains(x, set) ? set : [x] + set
 }
 
+enum Tree<T> {
+    case Leaf
+    case Node(Box<Tree<T>>, Box<T>, Box<Tree<T>>)
+}
+
+class Box<T> {
+    let unbox: T
+    init(_ value: T) { self.unbox = value }
+}
 
 
+let leaf: Tree<Int> = Tree.Leaf
 
-
+let five: Tree<Int> = Tree.Node(Box(leaf), Box(5), Box(leaf))
 
 
 
