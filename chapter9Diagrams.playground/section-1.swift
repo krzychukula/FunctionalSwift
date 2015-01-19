aaba96f90a0e8439a8b5240e812691d4dedbe2ef
krzychukula <krzychukula@gmail.com>
2015-01-19 08:46:03 +0100

    I give up. This chapter is a mess

--- chapter9Diagrams.playground/section-1.swift
+++ chapter9Diagrams.playground/section-1.swift
@@ -2,4 +2,89 @@
 
 import Cocoa
 
+//The Core Data Structures
 
+enum Primitive {
+    case Ellipse
+    case Rectangle
+    case Text(String)
+}
+
+class Box<T> {
+    let unbox: T
+    init(_ value: T) { self.unbox = value }
+}
+
+// A 2-D Vector
+struct Vector2D {
+    let x: CGFloat
+    let y: CGFloat
+    var point : CGPoint { return CGPointMake(x, y) }
+    
+    var size : CGSize { return CGSizeMake(x, y) }
+}
+
+enum Diagram {
+    case Prim(CGSize, Primitive)
+    case Beside(Box<Diagram>, Box<Diagram>)
+    case Below(Box<Diagram>, Box<Diagram>)
+    case Attributed(Attribure, Box<Diagram>)
+    case Align(Vector2D, Box<Diagram>)
+}
+
+enum Attribure {
+    case FillColor(NSColor)
+}
+
+extension Diagram {
+    var size: CGSize {
+        switch self {
+        case .Prim(let size, _):
+            return size
+        case .Attributed(_, let x):
+            return x.unbox.size
+        case .Beside(let l, let r):
+            let sizeL = l.unbox.size
+            let sizeR = r.unbox.size
+            return CGSizeMake(sizeL.width + sizeR.width, max(sizeL.height, sizeR.height))
+        case .Below(let l, let r):
+            let sizeL = l.unbox.size
+            let sizeR = r.unbox.size
+            return CGSizeMake(max(sizeL.width, sizeR.width), sizeL.height + sizeR.height)
+        case .Align(_, let r):
+            return r.unbox.size
+        }
+    }
+}
+
+func pointWise(f: (CGFloat, CGFloat) -> CGFloat,
+    l: CGPoint, r:CGPoint) -> CGPoint {
+        
+        return CGPointMake(f(l.x, r.x), f(l.y, r.y))
+}
+
+extension CGSize {
+    var point : CGPoint {
+        return CGPointMake(self.width, self.height)
+    }
+}
+
+func /(l: CGSize, r: CGSize) -> CGSize {
+    return pointWise(/, l, r)
+}
+func *(l: CGSize, r: CGSize) -> CGSize {
+    return pointWise(*, l, r)
+}
+func +(l: CGSize, r: CGSize) -> CGSize {
+    return pointWise(+, l, r)
+}
+func -(l: CGSize, r: CGSize) -> CGSize {
+    return pointWise(-, l, r)
+}
+
+
+func fit(alignment: Vector2D, inputSize: CGSize, rect: CGRect) -> CGRect {
+    let scaleSize = rect.size / inputSize
+    let scale = min(scaleSize.width, scaleSize.height)
+    
+}
