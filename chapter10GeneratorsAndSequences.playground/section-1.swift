4404da1bb68840566e119b27ef04f61772a9a8df
krzychukula <krzychukula@gmail.com>
2015-01-21 08:32:08 +0100

    simple generator returning indices

--- chapter10GeneratorsAndSequences.playground/section-1.swift
+++ chapter10GeneratorsAndSequences.playground/section-1.swift
@@ -2,4 +2,34 @@
 
 import Cocoa
 
+//var xs = [];
 
+//for x in xs {
+//    //do something with x;
+//}
+
+protocol GeneratorType {
+    typealias Element
+    func next() -> Element?
+}
+
+class CountdownGenerator: GeneratorType {
+    typealias Element = Int
+    
+    var element: Element
+    
+    init<T>(array: [T]){
+        self.element = array.count - 1
+    }
+    
+    func next() -> Element? {
+        return self.element < 0 ? nil : element--
+    }
+}
+
+let xs = ["A", "B", "C"]
+
+let generator = CountdownGenerator(array: xs)
+while let i = generator.next() {
+    println("Element \(i) of the array is \(xs[i])")
+}
