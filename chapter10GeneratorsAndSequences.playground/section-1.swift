cb7aeddcb32f9186dd9abca5fe1b1a00b20f08d4
krzychukula <krzychukula@gmail.com>
2015-01-22 08:38:20 +0100

    generator returning lines from a file

--- chapter10GeneratorsAndSequences.playground/section-1.swift
+++ chapter10GeneratorsAndSequences.playground/section-1.swift
@@ -58,3 +58,27 @@ func findPower(predicate: NSDecimalNumber -> Bool) -> NSDecimalNumber {
 
 findPower { $0.integerValue > 1000 }
 
+class FileLinesGenerator: GeneratorType {
+    typealias Element = String
+    
+    var lines: [String]
+    
+    init(filename: String){
+        if let contents = String(contentsOfFile: filename, encoding: NSUTF8StringEncoding, error: nil){
+            let newLine = NSCharacterSet.newlineCharacterSet()
+            lines = contents.componentsSeparatedByCharactersInSet(newLine)
+        }else{
+            lines = []
+        }
+    }
+    
+    func next() -> Element? {
+        if let nextLine = lines.first{
+            lines.removeAtIndex(0)
+            return nextLine
+        }else{
+            return nil
+        }
+    }
+}
+
