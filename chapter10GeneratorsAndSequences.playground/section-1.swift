906d0022f6ba157c23f0cbdd57fbafd8e9d4e037
krzychukula <krzychukula@gmail.com>
2015-01-22 08:41:17 +0100

    generic find in generator

--- chapter10GeneratorsAndSequences.playground/section-1.swift
+++ chapter10GeneratorsAndSequences.playground/section-1.swift
@@ -82,3 +82,11 @@ class FileLinesGenerator: GeneratorType {
     }
 }
 
+func find<G: GeneratorType>(var generator: G, predicate: G.Element -> Bool) -> G.Element? {
+    while let x = generator.next() {
+        if predicate(x) {
+            return x
+        }
+    }
+    return nil
+}
