705a857d13d54e37c0ade0c954e55e1d4fcf0ee9
krzychukula <krzychukula@gmail.com>
2015-01-26 08:32:54 +0100

    choice

--- chpater11ParserCombinators.playground/section-1.swift
+++ chpater11ParserCombinators.playground/section-1.swift
@@ -55,4 +55,20 @@ func token<Token: Equatable>(t: Token) -> Parser<Token, Token> {
 }
 
 
+//Choice
+
+infix operator <|> { associativity right precedence 130 }
+func <|> <Token, A>(l: Parser<Token, A>,
+    r: Parser<Token, A>) -> Parser<Token, A> {
+        
+        return Parser { input in
+            l.p(input) + r.p(input)
+        }
+}
+
+
+let a: Character = "a"
+let b: Character = "b"
+
+testParser(token(a) <|> token(b), "bcd")
 
