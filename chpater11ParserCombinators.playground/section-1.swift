6beefc7c8d0dbf63e2ddd160e8ff64698e44b1ad
krzychukula <krzychukula@gmail.com>
2015-01-27 08:06:23 +0100

    Sequence

--- chpater11ParserCombinators.playground/section-1.swift
+++ chpater11ParserCombinators.playground/section-1.swift
@@ -72,3 +72,32 @@ let b: Character = "b"
 
 testParser(token(a) <|> token(b), "bcd")
 
+
+
+//Sequence
+
+
+func sequence<Token, A, B>(l: Parser<Token, A>,
+    r: Parser<Token, B>) -> Parser<Token, (A, B)> {
+        
+        return Parser { input in
+            let leftResults = l.p(input)
+            return flatMap(leftResults) { a, leftRest in
+                let rightResults = r.p(leftRest)
+                return map(rightResults, { b, rightRest in
+                    ((a, b), rightRest)
+                })
+            }
+        }
+}
+
+
+let x: Character = "x"
+let y: Character = "y"
+let p: Parser<Character, (Character, Character)> = sequence(token(x), token(y))
+testParser(p, "xyz")
+
+
+
+
+
